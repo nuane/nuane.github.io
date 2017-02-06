fd9ba345e91e3dcbd14d92c397430e890a990b5b
nuane9000 <nuance9000@gmail.com>
2017-02-06 04:12:26 -0800

    mathbubble and design stuff

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,9 +4,50 @@
 	jQuery(document).ready(function(){
 		jQuery('a[data-gal]').each(function() {
 			jQuery(this).attr('rel', jQuery(this).data('gal'));
-		});  	
-		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
-	}); 
+		});
+		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({
+			animationSpeed:'normal',
+			theme:'dark_rounded',
+			slideshow:false,
+			overlay_gallery: false,
+			social_tools:false,
+			deeplinking:false
+		});
+	});
 
-		
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('.navbar').outerHeight();
+
+	$(window).scroll(function(event){
+		didScroll = true;
+	});
+
+	setInterval(function() {
+		if (didScroll){
+			hasScrolled();
+			didScroll = false;
+		}
+	}, 250);
+
+	function hasScrolled(){
+		var st = $(this).scrollTop();
+		if (Math.abs(lastScrollTop - st) <= delta) {
+			return;
+		}
+		// If current position > last position AND scrolled past navbar...
+		if (st > lastScrollTop && st > navbarHeight){
+		  // Scroll Down
+		  $('.navbar').removeClass('nav-down').addClass('nav-up');
+		} else {
+		  // Scroll Up
+		  // If did not scroll past the document (possible on mac)...
+		  if(st + $(window).height() < $(document).height()) {
+		    $('.navbar').removeClass('nav-up').addClass('nav-down');
+		  }
+		}
+		lastScrollTop = st;
+	}
+
 })(jQuery);
